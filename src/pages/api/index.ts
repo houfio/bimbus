26dc5301a8a51305735b8813b851484495e1c9ed
houfio <26674115+houfio@users.noreply.github.com>
2021-02-15 21:31:45 +0100

    Add api base url

---
+++ src/pages/api/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({
+    status: {
+      success: true,
+      error: null
+    },
+    data: {
+      version: 1
+    }
+  });
+}
